2cdeabe0ca12724db2429438158fc4f8f2822874
Darius Atlas <mit.3tlasa@gmail.com>
2021-06-20 22:23:50 -0700

    addInputAndOutputVars

--- venv/main.py
+++ venv/main.py
@@ -4,3 +4,9 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 
 import numpy as np
 
+celsius_q    = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
+fahrenheit_a = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
+
+for i,c in enumerate(celsius_q):
+  print("{} градусов Цельсия = {} градусов Фаренгейта".format(c, fahrenheit_a[i]))
+
