9259e537f1f31585ae0f0e782c450eec6319274a
Darius Atlas <mit.3tlasa@gmail.com>
2021-06-20 22:24:15 -0700

    add layer for Dense

--- venv/main.py
+++ venv/main.py
@@ -10,3 +10,9 @@ fahrenheit_a = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 for i,c in enumerate(celsius_q):
   print("{} градусов Цельсия = {} градусов Фаренгейта".format(c, fahrenheit_a[i]))
 
+
+
+l0 = tf.keras.layers.Dense(units=1, input_shape=[1])
+
+
+
